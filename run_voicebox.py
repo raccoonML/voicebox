429247b0502f9dfdd3851996835a94dc758d6aa2
raccoonML <95999607+raccoonML@users.noreply.github.com>
2021-12-30 01:17:10 -0800

    Add sample vocoder project.

--- run_voicebox.py
+++ run_voicebox.py
@@ -1,5 +1,8 @@
-from voicebox.vc import Voicebox
-from sample_project import Sample_Project
+import argparse
+from examples.vc.sample_vc_project import Sample_Project as Sample_VC_Project
+from examples.vocoder.sample_vocoder_project import Sample_Project as Sample_Vocoder_Project
+from voicebox.vc import Voicebox as Voicebox_VC
+from voicebox.vocoder import Voicebox as Voicebox_Vocoder
 
 
 """
@@ -12,9 +15,25 @@ The project needs to provide some interfaces to Voicebox to integrate with the U
 See sample_project.py for a minimal example of those interfaces.
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--vc", action="store_true", help=\
+            "Launches a sample voice conversion project.")
+    parser.add_argument("--vocoder", action="store_true", help=\
+            "Launches a sample vocoder project.")
+    args = parser.parse_args()
+
+    if args.vc:
+        Sample_Project = Sample_VC_Project
+        Voicebox = Voicebox_VC
+        window_title = "Sample Voice Conversion Project"
+    elif args.vocoder:
+        Sample_Project = Sample_Vocoder_Project
+        Voicebox = Voicebox_Vocoder
+        window_title = "Sample Vocoder Project"
+    
     # Initialize the project
     sample_project = Sample_Project()
 
     # Start voice conversion UI for project
-    Voicebox(sample_project, window_title="Sample Project")
+    Voicebox(sample_project, window_title=window_title)
